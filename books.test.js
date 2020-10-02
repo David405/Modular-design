0867e6ba6850052a7931e75f0e36f31de63fad6f
David Asamonye <davidasamonye@Davidc.local>
2020-10-03 00:02:11 +0100

    added describe

--- books.test.js
+++ books.test.js
@@ -1,20 +1,28 @@
 const functions = require('./functions');
 
-test('Display the list of books', () => {
-    books = ["The Potter's Wheel", "Sugar Girl", "My Father's Car"];
-    expect(functions.display()).toStrictEqual(books);
+describe('Display function', () => {
+    test('Display the list of books', () => {
+        books = ["The Potter's Wheel", "Sugar Girl", "My Father's Car"];
+        expect(functions.display()).toStrictEqual(books);
+    });
 });
 
-test('Check if new book is added to Bookstore', () => {
-    book = "new book";
-    expect(functions.add()).toContain(book);
+describe('Add function', () => {
+    test('Check if new book is added to Bookstore', () => {
+        book = "new book";
+        expect(functions.add()).toContain(book);
+    });
 });
 
-test('Check if book is deleted from bookStore', () => {
-    book = "Sugar Girl";
-    expect(functions.lend(2)).toBe(book);
+describe('Lend function', () => {
+    test('Check if book is deleted from bookStore', () => {
+        book = "Sugar Girl";
+        expect(functions.lend(2)).toBe(book);
+    });
 });
 
-test('Check for the number of lentbooks', () => {
-    expect(functions.track(1)).toBe("1 book(s) has been rented")
+describe('Track function', () => {
+    test('Check for the number of lentbooks', () => {
+        expect(functions.track(1)).toBe("1 book(s) has been rented")
+    });
 });
